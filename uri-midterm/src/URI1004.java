ea3b3a42786dfcd1cc5742101eacd002ec52349a
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-09-29 13:08:02 +0600

    uri1004 - accepted

---
+++ uri-midterm/src/URI1004.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class URI1004 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt(), b = scan.nextInt();
+        int prod = a*b;
+        System.out.println("PROD = " + prod);
+    }
+}
