a68cdeebb4f5dabf4384dece0e53d5ba40519433
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-09-29 13:04:57 +0600

    uri1003 - accepted

---
+++ uri-midterm/src/URI1003.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class URI1003 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt(), b = scan.nextInt();
+        int soma = a+b;
+        System.out.println("SOMA = " + soma);
+    }
+}
