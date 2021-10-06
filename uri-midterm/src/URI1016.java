f46d92dabdeee66b59172e2ac59a0070f623e5fd
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-10-06 13:06:36 +0600

    uri1016 - Accepted

---
+++ uri-midterm/src/URI1016.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class URI1016 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int distance = scan.nextInt();
+        System.out.println(distance*2 + " minutos");
+    }
+}
