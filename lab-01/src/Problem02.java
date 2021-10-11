87a65a1722b6e6784a605e20925870f9fa4e2597
kazybekov-a-auca-2021 <90751549+kazybekov-a-auca-2021@users.noreply.github.com>
2021-10-11 23:07:53 +0600

    I like programming

--- lab-01/src/Problem02.java
+++ lab-01/src/Problem02.java
@@ -1,6 +1,5 @@
 public class Problem02 {
     public static void main (String[] args) {
-        //code
         System.out.println("*********************");
         System.out.println("*I like programming!*");
         System.out.println("*********************");
