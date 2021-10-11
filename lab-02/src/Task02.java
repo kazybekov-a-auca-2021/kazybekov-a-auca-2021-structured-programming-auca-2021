4047bb20bd30f5849dc9f622ed5b452df6dd05cd
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-10-11 13:44:38 +0600

    swap two numbers without temporary variable

--- lab-02/src/Task02.java
+++ lab-02/src/Task02.java
@@ -9,6 +9,7 @@ public class Task02 {
         int b = scan.nextInt();
         System.out.printf("Before swapping a = %d; b = %d;\n",a,b);
         a = a+b; b = a-b; a = a-b;
+        //low
         System.out.printf("After swapping a = %d; b = %d;\n",a,b);
     }
 }
