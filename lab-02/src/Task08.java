2e9e39b31994f526922c35ee82a0bfb9c426a8ce
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-10-13 13:05:44 +0600

    lab02-task08 pounds to kilograms

---
+++ lab-02/src/Task08.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Task08 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number in pounds: ");
+        double pounds = scan.nextDouble();
+        double kilo = pounds*0.454;
+        System.out.printf("%.1f pounds is %.3f kilograms%n",pounds,kilo);
+
+    }
+}
