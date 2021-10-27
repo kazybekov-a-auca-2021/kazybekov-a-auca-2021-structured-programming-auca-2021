74229b7600922e6f004459e0ead5baa0ed2ff707
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-10-27 13:08:32 +0600

    Project 01 - Processing

--- project-01/src/Problem01.java
+++ project-01/src/Problem01.java
@@ -3,10 +3,9 @@ import processing.core.*;
 import java.util.Random;
 
 public class Problem01 extends PApplet {
-    Random random = new Random();
 
     static float MIN_SIZE = 10;
-    static float MAX_SIZE = 146;
+    static float MAX_SIZE = 90;
 
     static String MESSAGE = "Hello, Processing!!!";
 
@@ -14,7 +13,7 @@ public class Problem01 extends PApplet {
     int red, green, blue;
 
     public void settings() {
-        fullScreen();
+        size(700, 700);
     }
 
     public void setup() {
@@ -23,26 +22,34 @@ public class Problem01 extends PApplet {
         green = 0;
         blue = 0;
     }
+
     public void draw() {
         background(0, 0, 0);
         textSize(messageSize);
         textAlign(CENTER, CENTER);
         fill(red, green, blue);
         text(MESSAGE, width / 2f, height / 2f);
-        if (messageSize < MAX_SIZE-1) {
-            messageSize += 1;
-        }
-        else if(messageSize>MIN_SIZE+1){
-            messageSize -= 1;
-            MAX_SIZE-=1;
-        }
-        else {
-            MAX_SIZE=146;
-            if(red==255) {green = 255; red=0;}
-            else if(green==255) {blue = 255; green=0;}
-            else if(blue==255) {red=255; blue=0;}
+
+        if (messageSize < MAX_SIZE - 1) {
+            messageSize += 2;
+        } else if (messageSize > MIN_SIZE + 1) {
+            messageSize -= 2;
+            MAX_SIZE -= 2;
+        } else {
+            MAX_SIZE = 90;
+            if (red == 255) {
+                green = 255;
+                red = 0;
+            } else if (green == 255) {
+                blue = 255;
+                green = 0;
+            } else {
+                red = 255;
+                blue = 0;
+            }
         }
     }
+
     public static void main(String[] args) {
         PApplet.main("Problem01");
     }
