16fe060fa4539bda7bb0774a01e18a1481d61333
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-02 14:38:44 +0600

    mini calculator

--- lab-01/src/Problem04.java
+++ lab-01/src/Problem04.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Problem04 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         //code
         Scanner scan = new Scanner(System.in);
         System.out.print("First int: ");
@@ -9,11 +9,11 @@ public class Problem04 {
         System.out.print("Second int: ");
         int b = scan.nextInt();
 
-        int sum = a+b;
-        int sub = a-b;
-        int prod = a*b;
-        int div = a/b;
-        int mod = a%b;
+        int sum = a + b;
+        int sub = a - b;
+        int prod = a * b;
+        int div = a / b;
+        int mod = a % b;
         System.out.println(a + " + " + b + " = " + sum);
         System.out.println(a + " - " + b + " = " + sub);
         System.out.println(a + " * " + b + " = " + prod);
