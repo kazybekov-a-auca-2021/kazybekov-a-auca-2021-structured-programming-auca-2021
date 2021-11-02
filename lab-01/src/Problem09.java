7c28bd393a1b200c0efa0630602dd83a317c7c97
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-02 14:43:26 +0600

    calculating irrational number PI

--- lab-01/src/Problem09.java
+++ lab-01/src/Problem09.java
@@ -1,9 +1,9 @@
 public class Problem09 {
     public static void main(String[] args) {
-        double pi , pi1;
-        double initial = (1.0 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+        double pi, pi1;
+        double initial = (1.0 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11));
         pi = 4 * initial;
-        pi1 = 4 * (initial + (1.0/13));
+        pi1 = 4 * (initial + (1.0 / 13));
         System.out.println(pi + " " + pi1);
     }
 }
