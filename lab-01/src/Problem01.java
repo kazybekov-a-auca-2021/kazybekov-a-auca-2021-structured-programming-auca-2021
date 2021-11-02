4192482742e8a33ee11d65a83426686c0cc49ac7
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-02 14:34:36 +0600

    Hello world!

--- lab-01/src/Problem01.java
+++ lab-01/src/Problem01.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Problem01 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
