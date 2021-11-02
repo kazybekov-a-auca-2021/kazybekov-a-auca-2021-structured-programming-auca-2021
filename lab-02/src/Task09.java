d92e937c5cb161c2fba07e316a6ca5d6397ef63b
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-02 14:50:26 +0600

    minutes in years and days

--- lab-02/src/Task09.java
+++ lab-02/src/Task09.java
@@ -6,10 +6,10 @@ public class Task09 {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the number of minutes: ");
         int minutes = scan.nextInt();
-        int hours = minutes/60;
-        int days = hours/24;
-        int years = days/365;
-        days%=365;
-        System.out.printf("%d minutes is approximately %d years and %d days%n",minutes,years,days);
+        int hours = minutes / 60;
+        int days = hours / 24;
+        int years = days / 365;
+        days %= 365;
+        System.out.printf("%d minutes is approximately %d years and %d days%n", minutes, years, days);
     }
 }
