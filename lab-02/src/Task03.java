e1f89a866427de612a1e1de9041d56bd2b396bf6
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-02 14:46:39 +0600

    sum of digits of four-digit number

--- lab-02/src/Task03.java
+++ lab-02/src/Task03.java
@@ -5,13 +5,13 @@ public class Task03 {
         Scanner scan = new Scanner(System.in);
         System.out.print("A four-digit integer? ");
         int n = scan.nextInt();
-        int a = n/1000;
-        n%=1000;
-        int b = n/100;
-        n%=100;
-        int c = n/10;
-        n%=10;
-        int sum = a+b+c+n;
+        int a = n / 1000;
+        n %= 1000;
+        int b = n / 100;
+        n %= 100;
+        int c = n / 10;
+        n %= 10;
+        int sum = a + b + c + n;
         System.out.println("The sum of all digits is " + sum);
     }
 }
