35dbb66d713da70c8fa150bf0472c56b5d2362c1
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-02 14:50:47 +0600

    population grows with given input

--- lab-02/src/Task10.java
+++ lab-02/src/Task10.java
@@ -5,11 +5,11 @@ public class Task10 {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the number of years: ");
         int years = scan.nextInt();
-        int seconds = 31536000*years;
-        int birth = seconds/7;
-        int death = seconds/13;
-        int immigrant = seconds/45;
-        int result = 312032486+birth-death+immigrant;
-        System.out.printf("The population in %d years is %d",years,result);
+        int seconds = 31536000 * years;
+        int birth = seconds / 7;
+        int death = seconds / 13;
+        int immigrant = seconds / 45;
+        int result = 312032486 + birth - death + immigrant;
+        System.out.printf("The population in %d years is %d", years, result);
     }
 }
