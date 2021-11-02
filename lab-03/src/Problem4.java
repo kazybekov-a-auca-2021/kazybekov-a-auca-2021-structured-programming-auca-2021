16043d3d89e9672ceb369c299c9b5551acbba77f
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-02 14:53:03 +0600

    leap year

--- lab-03/src/Problem4.java
+++ lab-03/src/Problem4.java
@@ -7,7 +7,10 @@ public class Problem4 {
         System.out.print("Year? ");
         int year = scan.nextInt();
 
-        if(year%400==0 || (year%4==0 && year%100!=0)) System.out.println("This is a leap year.");
-        else System.out.println("This is not a leap year.");
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println("This is a leap year.");
+        } else {
+            System.out.println("This is not a leap year.");
+        }
     }
 }
