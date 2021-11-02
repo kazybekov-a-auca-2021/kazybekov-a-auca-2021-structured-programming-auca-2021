a38b19e58e19a8b6485358908245ce5a06ae368a
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-02 14:53:54 +0600

    grading the student

--- lab-03/src/Problem5.java
+++ lab-03/src/Problem5.java
@@ -7,11 +7,18 @@ public class Problem5 {
         System.out.print("Number of points? ");
         int points = scan.nextInt();
 
-        if(points>=90 && points<=100) System.out.println("Grade: A");
-        else if(points>=80 && points<90) System.out.println("Grade: B");
-        else if(points>=70 && points<80) System.out.println("Grade: C");
-        else if(points>=60 && points<70) System.out.println("Grade: D");
-        else if(points>=0 && points<60) System.out.println("Grade: F");
-        else System.out.println(points + " is not in the permitted range.");
+        if (points >= 90 && points <= 100) {
+            System.out.println("Grade: A");
+        } else if (points >= 80 && points < 90) {
+            System.out.println("Grade: B");
+        } else if (points >= 70 && points < 80) {
+            System.out.println("Grade: C");
+        } else if (points >= 60 && points < 70) {
+            System.out.println("Grade: D");
+        } else if (points >= 0 && points < 60) {
+            System.out.println("Grade: F");
+        } else {
+            System.out.println(points + " is not in the permitted range.");
+        }
     }
 }
