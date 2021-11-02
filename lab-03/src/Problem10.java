0d16f8c204a30974320749ed3fb189aa7870a95d
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-02 15:34:54 +0600

    is point in the circle?

---
+++ lab-03/src/Problem10.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Problem10 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a point with two coordinates: ");
+        double x = scan.nextDouble();
+        double y = scan.nextDouble();
+
+        double distance = Math.sqrt(x * x + y * y);
+
+        if (distance <= 10) {
+            System.out.printf("Point (%.1f, %.1f) is in the circle", x, y);
+        } else {
+            System.out.printf("Point (%.1f, %.1f) is not in the circle", x, y);
+        }
+    }
+}
