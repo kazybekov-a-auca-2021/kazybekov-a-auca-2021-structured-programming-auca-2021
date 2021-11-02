11d56786bc17d9e712dab887f2c9efba79b1b49a
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-02 14:48:48 +0600

    fahrenheit to celsius

--- lab-02/src/Task05.java
+++ lab-02/src/Task05.java
@@ -5,7 +5,7 @@ public class Task05 {
         Scanner scan = new Scanner(System.in);
         System.out.print("Temperature in degrees Fahrenheit? ");
         double fah = scan.nextDouble();
-        double celcius = (fah-32)*(5.0/9.0);
-        System.out.printf("The temperature in degrees Celsius is %.2f",celcius);
+        double celsius = (fah - 32) * (5.0 / 9.0);
+        System.out.printf("The temperature in degrees Celsius is %.2f", celsius);
     }
 }
