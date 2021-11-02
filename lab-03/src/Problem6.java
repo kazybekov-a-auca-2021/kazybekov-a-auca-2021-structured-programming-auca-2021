5476aa8cca0b377467803061d9298a64cae4f719
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-02 14:54:17 +0600

    roots of the equation

--- lab-03/src/Problem6.java
+++ lab-03/src/Problem6.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class Problem6 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         System.out.print("Enter a, b, c: ");
         double a = scan.nextDouble(), b = scan.nextDouble(), c = scan.nextDouble();
         double dis = (b * b) - (4.0 * a * c);
+
         if (dis < 0 || a == 0) System.out.println("The equation has no real roots");
         else if (dis > 0) {
             dis = Math.sqrt(dis);
