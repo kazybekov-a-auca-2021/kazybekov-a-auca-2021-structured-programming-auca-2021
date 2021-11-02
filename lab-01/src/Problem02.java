46febc2537781eaf4fc58c113fc3e3bfd659b56a
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-02 14:35:25 +0600

    I like programming second problem of lab-1

--- lab-01/src/Problem02.java
+++ lab-01/src/Problem02.java
@@ -1,5 +1,5 @@
 public class Problem02 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("*********************");
         System.out.println("*I like programming!*");
         System.out.println("*********************");
