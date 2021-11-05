e33a5a8ee0ace68c6ac4873965a99fb2cadc3490
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-05 15:15:34 +0600

    URI 1047 - accepted

--- uri-midterm/src/URI1047.java
+++ uri-midterm/src/URI1047.java
@@ -7,18 +7,36 @@ public class URI1047 {
         int m1 = scan.nextInt();
         int h2 = scan.nextInt();
         int m2 = scan.nextInt();
+        int totalHours;
+        int totalMinutes;
+        if (h1 == h2) {
+            if (m2 <= m1) {
+                totalHours = h2 - h1;
+                if (totalHours <= 0) {
+                    totalHours = 24 + totalHours;
+                }
 
-        int totalHours = h2-h1;
-        if (totalHours<=0){
-            totalHours = 24+totalHours;
+                totalMinutes = m2 - m1;
+                if (totalMinutes < 0) {
+                    totalMinutes = 60 + totalMinutes;
+                    totalHours -= 1;
+                }
+                System.out.printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", totalHours, totalMinutes);
+            } else {
+                System.out.printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", 0, m2 - m1);
+            }
+        } else {
+            totalHours = h2 - h1;
+            if (totalHours <= 0) {
+                totalHours = 24 + totalHours;
+            }
+
+            totalMinutes = m2 - m1;
+            if (totalMinutes < 0) {
+                totalMinutes = 60 + totalMinutes;
+                totalHours -= 1;
+            }
+            System.out.printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", totalHours, totalMinutes);
         }
-
-        int totalMinutes = m2-m1;
-        if (totalMinutes<0){
-            totalMinutes = 60+totalMinutes;
-            totalHours-=1;
-        }
-
-        System.out.printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n",totalHours,totalMinutes);
     }
 }
