e0bc3343f7374df8b4390812033f675f117baf66
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-10 12:51:08 +0600

    determine equality

---
+++ lab-04/src/Problem01.java
@@ -0,0 +1,11 @@
+public class Problem01 {
+    public static void main(String[] args) {
+        double d = 0.1;
+        d *= 10;
+        if (d == 1.0) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not equal");
+        }
+    }
+}
