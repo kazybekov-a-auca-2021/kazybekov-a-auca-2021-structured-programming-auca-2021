fd40b42e4c1c7195f3b0c101bd4488135d7e7eca
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-13 17:09:01 +0600

    wrong answer

--- uri-midterm/src/URI1051.java
+++ uri-midterm/src/URI1051.java
@@ -4,18 +4,18 @@ public class URI1051 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         double value = scan.nextDouble();
-        if(value<=2000.0){
+        if (value <= 2000.0) {
             System.out.println("Isento");
-        } else if(value>2000.0 && value<=3000.0){
-            System.out.printf("R$ %.2f\n",value*0.08);
-        } else if(value>3000.0 && value<=4500.0){
-            double in = 1000*0.08;
-            double next = (value-3000.0)*0.18;
-            System.out.printf("R$ %.2f\n",next+in);
+        } else if (value > 2000.0 && value <= 3000.0) {
+            System.out.printf("R$ %.2f\n", value * 0.08);
+        } else if (value > 3000.0 && value <= 4500.0) {
+            double in = 1000 * 0.08;
+            double next = (value - 3000.0) * 0.18;
+            System.out.printf("R$ %.2f\n", next + in);
         } else {
-            double in = 1500 *0.18;
-            double next = (value-4500.0)*0.28;
-            System.out.printf("R$ %.2f\n",next+in);
+            double in = 1500 * 0.18;
+            double next = (value - 4500.0) * 0.28;
+            System.out.printf("R$ %.2f\n", next + in);
         }
     }
 }
