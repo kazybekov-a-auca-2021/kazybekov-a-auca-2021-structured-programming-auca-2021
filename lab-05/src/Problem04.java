3faa90aaff205030b9ed3ed5d9ad51243c6f9fec
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-24 13:14:57 +0600

    Random balls

--- lab-05/src/Problem04.java
+++ lab-05/src/Problem04.java
@@ -2,30 +2,29 @@ import processing.core.*;
 
 public class Problem04 extends PApplet {
 
-    int numberOfCircle = (int)(Math.random()*10);
+    final int MAX_NUMBER = 100;
+    final int MAX_RADIUS = 100;
+    final int COLOR_RANGE = 255;
 
     int x = 0, y = 0, radius = 0;
     int r = 0, g = 0, b = 0;
 
     public void settings() {
-       fullScreen();
+        fullScreen();
     }
 
     public void setup() {
-        x = 0;
-        y = 0;
-        radius = 0;
-        r = 0;
-        g = 0;
-        b = 0;
-    }
-
-    public void draw() {
-        background(0,0,0);
-        x = (int)(Math.random()*1920);
-        y = (int)(Math.random()*1080);
-        radius = (int)(Math.random()*100);
-        circle(x,y,radius);
+        background(0, 0, 0);
+        for (int i = 0; i < MAX_NUMBER; i++) {
+            x = (int) (Math.random() * width);
+            y = (int) (Math.random() * height);
+            radius = (int) (Math.random() * MAX_RADIUS);
+            r = (int) (Math.random() * COLOR_RANGE);
+            g = (int) (Math.random() * COLOR_RANGE);
+            b = (int) (Math.random() * COLOR_RANGE);
+            fill(r, g, b);
+            circle(x, y, radius);
+        }
     }
 
     public static void main(String[] args) {
