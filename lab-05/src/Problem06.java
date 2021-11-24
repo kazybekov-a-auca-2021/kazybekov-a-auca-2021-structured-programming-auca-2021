cb2973c6a8b474d49c2ba6b20b902598bcb2dc8c
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-24 13:01:51 +0600

    test do while

--- lab-05/src/Problem06.java
+++ lab-05/src/Problem06.java
@@ -4,13 +4,12 @@ public class Problem06 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Eneter an integer (the input ends if it is 0): ");
-        int n = scan.nextInt(), sum = n;
-        while (n != 0) {
+        int n = 0, sum = n;
+        do {
             System.out.print("Eneter an integer (the input ends if it is 0): ");
             n = scan.nextInt();
             sum += n;
-        }
+        } while (n != 0);
         System.out.printf("The sum is %d", sum);
     }
 }
