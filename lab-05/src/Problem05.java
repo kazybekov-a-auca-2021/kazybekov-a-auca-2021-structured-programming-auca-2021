68a2158bc8ede38646fe14a0b8efdc08118cc2b8
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-29 14:04:28 +0600

    red circle

--- lab-05/src/Problem05.java
+++ lab-05/src/Problem05.java
@@ -2,24 +2,31 @@ import processing.core.PApplet;
 
 public class Problem05 extends PApplet {
 
-    final int MAX_NUMBER = 14;
+    final int MAX_NUMBER = 16;
 
-    int radius = 0, alpha = 0;
+    float radius = 0;
+    float alpha = 0;
 
     public void settings() {
-        size(800, 800);
+        fullScreen();
     }
 
     public void setup() {
+        frameRate(2);
+        noStroke();
+    }
+
+    public void draw() {
         background(0, 0, 0);
         alpha = 100;
-        radius = 5;
+        radius = min(width, height) / 2f;
         for (int i = 1; i <= MAX_NUMBER; i++) {
             fill(255, 0, 0, alpha);
             circle(width / 2f, height / 2f, radius);
             radius += 35;
             alpha -= 5;
         }
+        noLoop();
     }
 
     public static void main(String[] args) {
