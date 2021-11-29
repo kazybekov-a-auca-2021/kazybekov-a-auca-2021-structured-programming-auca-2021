335eb4ec28bc0e6dc8e48f79672724f0c8df0e63
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-29 13:10:50 +0600

    number of tests updated

--- lab-05/src/Problem03.java
+++ lab-05/src/Problem03.java
@@ -6,20 +6,18 @@ public class Problem03 {
 
         System.out.print("Number of tests? ");
         int n = scan.nextInt();
-        int correct = 0, incorrect = 0;
+        int correct = 0;
 
         for (int i = 1; i <= n; i++) {
-            int a = (int) (Math.random() * 100);
-            int b = (int) (Math.random() * 100);
-            int c = (int) (Math.random() * 100);
-            System.out.println(a + " + " + b + " = " + c);
-            if (a + b == c) {
+            int a = (int) (Math.random() * 100) - 50;
+            int b = (int) (Math.random() * 100) - 50;
+            System.out.print(a + " + " + b + " = ");
+            int answer = scan.nextInt();
+            if (a + b == answer) {
                 correct++;
-            } else {
-                incorrect++;
             }
         }
         System.out.printf("Number of correct answers: %d\n", correct);
-        System.out.printf("Number of incorrect answers: %d\n", incorrect);
+        System.out.printf("Number of incorrect answers: %d\n", (n - correct));
     }
 }
