e2e09d222999e7e461e0c0be435b37996bfb1c4c
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-29 15:29:17 +0600

    Sequence IJ 2 - URI1096 is accepted

---
+++ uri-final/src/URI1096.java
@@ -0,0 +1,9 @@
+public class URI1096 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i += 2) {
+            System.out.printf("I=%d J=%d\n", i, 7);
+            System.out.printf("I=%d J=%d\n", i, 6);
+            System.out.printf("I=%d J=%d\n", i, 5);
+        }
+    }
+}
