af0019e2f891d79906ec56f18260e94738562d80
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-11-29 15:22:46 +0600

    Sequence IJ 1 - URI1095 is accepted

--- uri-final/src/URI1095.java
+++ uri-final/src/URI1095.java
@@ -1,11 +1,11 @@
 public class URI1095 {
     public static void main(String[] args) {
         int i = 1, j = 60;
-        System.out.printf("I=%d J=%d\n",i,j);
+        System.out.printf("I=%d J=%d\n", i, j);
         do {
-            i+=3;
-            j-=5;
-            System.out.printf("I=%d J=%d\n",i,j);
-        } while (j!=0);
+            i += 3;
+            j -= 5;
+            System.out.printf("I=%d J=%d\n", i, j);
+        } while (j != 0);
     }
 }
