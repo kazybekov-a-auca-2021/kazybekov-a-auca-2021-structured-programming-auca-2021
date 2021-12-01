e5aa81ea61ce0e2c8a43b207448bef030e452538
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-12-01 13:12:02 +0600

    URI1098 is accepted

--- uri-final/src/URI1098.java
+++ uri-final/src/URI1098.java
@@ -2,14 +2,15 @@ public class URI1098 {
     public static void main(String[] args) {
         double k = 1, l = 2, m = 3;
         for (double i = 0; i <= 20; i += 2) {
-            if((i/10==0.0) || (i/10==1.0) || (i/10==2.0)) {
-                System.out.printf("I=%.0f J=%.0f\n", i/10, k);
-                System.out.printf("I=%.0f J=%.0f\n", i/10, l);
-                System.out.printf("I=%.0f J=%.0f\n", i/10, m);
+            double j = i / 10;
+            if ((i / 10 == 0.0) || (i / 10 == 1.0) || (i / 10 == 2.0)) {
+                System.out.printf("I=%.0f J=%.0f\n", j, k);
+                System.out.printf("I=%.0f J=%.0f\n", j, l);
+                System.out.printf("I=%.0f J=%.0f\n", j, m);
             } else {
-                System.out.printf("I=%.1f J=%.1f\n", i/10, k);
-                System.out.printf("I=%.1f J=%.1f\n", i/10, l);
-                System.out.printf("I=%.1f J=%.1f\n", i/10, m);
+                System.out.printf("I=%.1f J=%.1f\n", j, k);
+                System.out.printf("I=%.1f J=%.1f\n", j, l);
+                System.out.printf("I=%.1f J=%.1f\n", j, m);
             }
             k += 0.2;
             l += 0.2;
