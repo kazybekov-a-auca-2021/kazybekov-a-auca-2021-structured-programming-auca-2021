2658b581aa096b8dbd982673733cae193b2b2d08
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-12-06 16:17:24 +0600

    PI table

--- lab-06/src/Problem09.java
+++ lab-06/src/Problem09.java
@@ -9,16 +9,15 @@ public class Problem09 {
     }
 
     public static void estimatedPI(double n) {
-        System.out.println("i            m(i)");
-        System.out.println("___________________");
-        System.out.printf("%.0f            %.4f\n", 1.0, 4.0);
-        for (double i = 101; i <= n; i += 100) {
+        System.out.println("   i          m(i)");
+        System.out.println("_____________________");
+        for (double i = 1; i <= n; i += 100) {
             double mI = 4;
             double sum = 0.0;
             for (double j = 1; j <= i; j++) {
                 sum += (Math.pow(-1, j + 1)) / (2 * j - 1);
             }
-            System.out.printf("%.0f          %.4f\n", i, mI * sum);
+            System.out.printf("%4.0f          %.4f\n", i, mI * sum);
         }
     }
 }
