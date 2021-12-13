843ec190e5dccecb3963d79fbd345f19f6cb41ec
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-12-13 14:05:32 +0600

    have not finished the 7th problem

--- lab-07/src/Problem07.java
+++ lab-07/src/Problem07.java
@@ -4,5 +4,22 @@ public class Problem07 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int[] arr = new int[101];
+
+        int number = scan.nextInt();
+
+        arr[number]++;
+        int sum = number , amount = 1;
+
+        while (number>=0){
+            number = scan.nextInt();
+            arr[number]++;
+            sum+=number;
+            amount++;
+        }
+
+        int average = sum/amount;
+
+
     }
 }
