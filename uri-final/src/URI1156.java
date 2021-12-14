44609bc1c38d61c80ac6504a4c337c85f9b77db9
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-12-14 12:27:41 +0600

    URI1156 is accepted

--- uri-final/src/URI1156.java
+++ uri-final/src/URI1156.java
@@ -1,13 +1,10 @@
-import java.util.Scanner;
-
 public class URI1156 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
         double s = 1, even = 2;
-        for (int i = 3; i <= 39 ; i+=2) {
-            s+=i/even;
-            even*=2;
+        for (int i = 3; i <= 39; i += 2) {
+            s += i / even;
+            even *= 2;
         }
-        System.out.printf("%.2f\n",s);
+        System.out.printf("%.2f\n", s);
     }
 }
