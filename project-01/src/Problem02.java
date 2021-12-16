ba59464110cc379354af86175695f473323e0807
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-12-16 15:38:10 +0600

    problem02 - update project 1

--- project-01/src/Problem02.java
+++ project-01/src/Problem02.java
@@ -6,10 +6,16 @@ public class Problem02 extends PApplet {
     float y = 0;
     float dx = 0;
     float dy = 0;
-    float a = 0;
-    float b = 0;
-    float da = 0;
-    float db = 0;
+
+    float x1 = 0;
+    float y1 = 0;
+    float dx1 = 0;
+    float dy1 = 0;
+
+    float x2 = 0;
+    float y2 = 0;
+    float dx2 = 0;
+    float dy2 = 0;
 
     public void settings() {
         fullScreen();
@@ -18,12 +24,18 @@ public class Problem02 extends PApplet {
     public void setup() {
         x = width / 2f;
         y = height / 2f;
-        a = width / 2f;
-        b = height / 2f;
-        dx = random(-20f, 20f);
-        dy = random(-20f, 20f);
-        da = dx;
-        db = dy;
+        dx = random(-20f,20f);
+        dy = random(-20f,20f);
+
+        x1 = width / 2f;
+        y1 = height / 2f;
+        dx1 = dx + 1;
+        dy1 = dy + 1;
+
+        x2 = width / 2f;
+        y2 = height / 2f;
+        dx2 = dx;
+        dy2 = dy;
     }
 
     public void draw() {
@@ -31,32 +43,69 @@ public class Problem02 extends PApplet {
         fill(255,0,0);
         circle(x, y, 50);
         fill(0,255,0);
-        circle(a, b, 50);
+        circle(x1, y1, 50);
+        fill(0,0,255);
+        circle(x2, y2, 50);
         x += dx;
         y += dy;
+        x1 += dx1;
+        y1 += dy1;
+        x2 += dx2;
+        y2 += dy2;
+
         if (x >= width) {
             x = width - 1;
-            a = width - 1;
             dx = -dx;
         }
         if (x < 0) {
             x = 0;
-            a = 0;
             dx = -dx;
         }
         if (y >= height) {
             y = height - 1;
-            b = height - 1;
             dy = -dy;
         }
         if (y < 0) {
             y = 0;
-            b = 0;
             dy = -dy;
         }
-        a += dx;
-        b += dy;
+
+        if (x1 >= width) {
+            x1 = width - 1;
+            dx1 = -dx1;
+        }
+        if (x1 < 0) {
+            x1 = 0;
+            dx1 = -dx1;
+        }
+        if (y1 >= height) {
+            y1 = height - 1;
+            dy1 = -dy1;
+        }
+        if (y1 < 0) {
+            y1 = 0;
+            dy1 = -dy1;
+        }
+
+        if (x2 >= width) {
+            x2 = width - 1;
+            dx2 = -dx2;
+        }
+        if (x2 < 0) {
+            x2 = 0;
+            dx2 = -dx2;
+        }
+        if (y2 >= height) {
+            y2 = height - 1;
+            dy2 = -dy2;
+        }
+        if (y2 < 0) {
+            y2 = 0;
+            dy2 = -dy2;
+        }
+
     }
+
 
     public static void main(String[] args) {
         PApplet.main("Problem02");
