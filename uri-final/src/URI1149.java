0a8e70b85c894610333a975a49c85e3635aa10a7
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-12-21 02:57:34 +0600

    PROBLEM: 1149 - Summing Consecutive Integers is accepted

---
+++ uri-final/src/URI1149.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class URI1149 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt(), n;
+
+        do {
+            n = scan.nextInt();
+        } while (n<=0);
+
+        int num = a;
+        n--;
+        while (n>0){
+            a++;
+            num+=a;
+            n--;
+        }
+
+        System.out.println(num);
+    }
+}
