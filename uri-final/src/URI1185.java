e85a300b06498fca1cbafac8ce2128d3994ea6a5
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-12-21 03:14:33 +0600

    PROBLEM: 1185 - Above the Secundary Diagonal ANSWER: Accepted

---
+++ uri-final/src/URI1185.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class URI1185 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int size = 12;
+        char ch = scan.next().charAt(0);
+
+        double[][] set = new double[size][size];
+        double sum = 0.0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                set[i][j] = scan.nextDouble();
+                if (j < 11 - i) {
+                    sum += set[i][j];
+                }
+            }
+        }
+
+        if (ch == 'S') {
+            System.out.printf("%.1f\n", sum);
+        } else {
+            System.out.printf("%.1f\n", (sum / 66.0));
+        }
+    }
+}
