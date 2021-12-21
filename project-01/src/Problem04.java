13a9d1e873282257e13ab8a7c1f1013f636a0932
Aidar Kazybekov <kazybekov_a@auca.kg>
2021-12-21 16:32:03 +0600

    attaching the field and ball

--- project-01/src/Problem04.java
+++ project-01/src/Problem04.java
@@ -2,34 +2,50 @@ import processing.core.*;
 
 public class Problem04 extends PApplet {
 
-    float horizontal, vertical;
+    final float FIELD = 700;
+
+    float rectX, rectY;
 
     float circleX, circleY, circleR;
-
-    float columns, rows, scale;
+    float amount = 20;
 
     public void settings() {
         fullScreen();
     }
 
     public void setup() {
-        circleX = circleR / 2f;
-        circleY = circleR / 2f;
-        circleR = height / 20f;
-        frameRate(10);
 
+        frameRate(10);
+        rectX = width / 2f - width / 4.65f;
+        rectY = height / 2f - height / 3.0f;
+        circleR = FIELD/amount;
+        circleX = rectX + circleR;
+        circleY = rectY + circleR;
+
+        amount = 12;
     }
 
     public void draw() {
         fill(0,0,0,50);
         rect(0,0,width,height);
 
+        noFill();
+        stroke(0,0,255);
+        rect(rectX,rectY,FIELD,FIELD);
+
+        for (int i = 0; i < amount; i++) {
+            for (int j = 0; j < amount; j++) {
+                float w = rectX + i * FIELD / amount;
+                float l = rectY + j * FIELD / amount;
+
+                square(w, l, FIELD / amount);
+            }
+        }
 
         fill(255,0,0);
         circle(circleX,circleY,circleR);
-
-        if (key == CODED) {
-            switch (keyCode) {
+        if (key == CODED){
+            switch (keyCode){
                 case UP -> circleY -= 2 * circleR;
                 case DOWN -> circleY += 2 * circleR;
                 case LEFT -> circleX -= 2 * circleR;
